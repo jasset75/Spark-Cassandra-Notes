8e43c73cdb873fd9b21a840ea3fb7241f8f9727b
Juan Antonio Aguilar <juanantonioaguilar@gmail.com>
2018-01-01 21:32:07 +0100

    minor changes

--- examples/mock-example/src/main/scala/ExampleTest.scala
+++ examples/mock-example/src/main/scala/ExampleTest.scala
@@ -13,7 +13,7 @@ object MockData {
     // spark context creation with conf
     val sc = new SparkContext(conf)
     //select from Cassandra Table with Cassandra-Scala type conversion
-    sc.cassandraTable[(String,String)]("centro_med","mock_data")
+    sc.cassandraTable[(String,String)]("examples","mock_data")
       .where("gender = 'Male'") //gender filtering
       .select("gender","first_name") //conver to RDD pair with gender and first_name columns
       .map{case (k,v) => (v,1)} //associate 1 point to each male first name
