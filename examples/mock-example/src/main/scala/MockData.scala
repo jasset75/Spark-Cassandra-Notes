2a8b7fb49efcf67bbd8643ff57afe6168cc537dd
Juan Antonio Aguilar <juanantonioaguilar@gmail.com>
2018-01-02 01:46:57 +0100

    example extended to female names

--- examples/mock-example/src/main/scala/MockData.scala
+++ examples/mock-example/src/main/scala/MockData.scala
@@ -12,15 +12,40 @@ object MockData {
       .set("spark.cassandra.connection.host", "localhost")
     // spark context creation with conf
     val sc = new SparkContext(conf)
-    //select from Cassandra Table with Cassandra-Scala type conversion
-    sc.cassandraTable[(String,String)]("examples","mock_data")
-      .where("gender = 'Male'") //gender filtering
-      .select("gender","first_name") //conver to RDD pair with gender and first_name columns
-      .map{case (k,v) => (v,1)} //associate 1 point to each male first name
-      .reduceByKey{case (v,count) => count + count} //count 
-      .sortByKey()
-      .collect
-      .foreach(println)
+    // reducing verbosity
+    sc.setLogLevel("ERROR")
+    // select from Cassandra Table with Cassandra-Scala type conversion
+    val record_names = sc.cassandraTable[(String,String)]("examples","mock_data")
+                        .select("gender","first_name") //convert to RDD pair with gender and first_name columns              
+                        .cache
+    //Male
+    val male_names = record_names.where("gender = 'Male'") //gender filtering 
+    val male_names_c = male_names.map{case (k,v) => (v,1)} //associate 1 point to each male first name
+              .cache //optimize several actions over RDD
+    val males_result = male_names_c.reduceByKey{case (v,count) => count + count} //count 
+    //Female
+    val female_names = record_names.where("gender = 'Female'") //gender filtering 
+    val female_names_c = female_names.map{case (k,v) => (v,1)} //associate 1 point to each male first name
+              .cache //optimize several actions over RDD
+    val females_result = female_names_c.reduceByKey{case (v,count) => count + count} //count 
+    //Male
+    println("Ordered Male Names count list:")
+    // ordered RDD by male names                            
+    val males_result_az = males_result.sortByKey() //key male names are sorted in asc order
+    males_result_az.collect.foreach(println) //print result records through stdout
+    // taking 5 highest male repeated names                   
+    println("Five highest repeated male names:")
+    val males_result_high = males_result.sortBy(_._2,false).take(5)
+    males_result_high.foreach(println)
+    //Female
+    println("Ordered Female Names count list:")
+    // ordered RDD by female names                            
+    val females_result_az = females_result.sortByKey() //key male names are sorted in asc order
+    females_result_az.collect.foreach(println) //print result records through stdout
+    // taking 5 highest female repeated names                   
+    println("Five highest repeated female names:")
+    val females_result_high = females_result.sortBy(_._2,false).take(5)
+    females_result_high.foreach(println)
     sc.stop()
   }
 }
