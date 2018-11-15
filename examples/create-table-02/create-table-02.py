221943bf78b569ae076bfe160253951ac3929e47
Juan Antonio Aguilar <juanantonioaguilar@gmail.com>
2018-11-15 23:01:15 +0100

    finishing example

--- examples/create-table-02/create-table-02.py
+++ examples/create-table-02/create-table-02.py
@@ -8,15 +8,19 @@ from cassandra.cqlengine.models import Model
 from cassandra.cqlengine.columns import *
 from cassandra.cqlengine.management import sync_table, create_keyspace_simple
 
-# setting up CQLENG_ALLOW_SCHEMA_MANAGEMENT to avoid warnings
-os.environ['CQLENG_ALLOW_SCHEMA_MANAGEMENT'] = 'yes'
-
 # pandas
-from pandas import read_csv, concat
+import pandas as pd
+
 # status bar for load progress
 from tqdm import tqdm
 
+# setting up locale
 locale.setlocale(locale.LC_ALL,'')
+
+# setting up CQLENG_ALLOW_SCHEMA_MANAGEMENT to avoid warnings
+if not os.getenv('CQLENG_ALLOW_SCHEMA_MANAGEMENT'):
+    os.environ['CQLENG_ALLOW_SCHEMA_MANAGEMENT'] = 'yes'
+
 
 # Apache Cassandra connection
 list_of_ip = ['127.0.0.1']
@@ -37,14 +41,15 @@ COLUMNS = ['car_id','registration','car_make','car_model','car_model_year','colo
 
 # Object Mapper
 class MockCars(Model):
-  __keyspace__ = NEW_KEYSPACE
-  car_id = UUID(primary_key=True)
-  registration = Text()
-  car_make = Text()
-  car_model = Text()
-  car_model_year = Integer()
-  color = Text()
-  id_owner = Integer()
+    __keyspace__ = NEW_KEYSPACE
+    __table_name__  = MOCK_DATA_TABLE
+    car_id = UUID(primary_key=True)
+    registration = Text()
+    car_make = Text()
+    car_model = Text()
+    car_model_year = Integer()
+    color = Text()
+    id_owner = Integer()
 
 
 # create keyspace
@@ -55,7 +60,7 @@ sync_table(MockCars)
 
 
 # reading data from csv file into pandas panel
-df = read_csv(os.path.abspath(FICHERO_DATOS),header=0,names=COLUMNS,quotechar='"',decimal=',',encoding=ENCODING)
+df = pd.read_csv(os.path.abspath(FICHERO_DATOS),header=0,names=COLUMNS,quotechar='"',decimal=',',encoding=ENCODING)
 
 # NaNs is not desired
 df.fillna(0,inplace=True)
@@ -63,13 +68,19 @@ df.fillna(0,inplace=True)
 
 # saving data to database
 for ind, row in tqdm(df.iterrows(), total=df.shape[0]):
-  MockCars.create(
-    car_id = row['car_id'],
-    registration = row['registration'],
-    car_make = row['car_make'],
-    car_model = row['car_model'],
-    car_model_year = row['car_model_year'],
-    color = row['color'],
-    id_owner = row['id_owner']
-  )
+    MockCars.create(
+        car_id = row['car_id'],
+        registration = row['registration'],
+        car_make = row['car_make'],
+        car_model = row['car_model'],
+        car_model_year = row['car_model_year'],
+        color = row['color'],
+        id_owner = row['id_owner']
+    )
 
+# read from cassandra into pandas dataframe
+
+query = 'SELECT * FROM {}.{}'.format(NEW_KEYSPACE, MOCK_DATA_TABLE)
+df = pd.DataFrame(list(session.execute(query)))
+
+print(df)
